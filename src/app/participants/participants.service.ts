5eb00a8294395bf090d12811dbc0eeab6566661f
dhain478 <dhainkarl12@gmail.com>
2021-09-29 15:53:11 +0800

    first commit

---
+++ src/app/participants/participants.service.ts
@@ -0,0 +1,15 @@
+export class ParticipantsService {
+    getParticipants(){
+        return [
+            {name: "Lexi Lore", age:22 + " years old", gender: "Female"},
+            {name: "lana Rhoades", age:24 + " years old", gender: "FeMale"},
+            {name: "Alejandro Magbanua", age:33 + " years old", gender: "Male"},
+            {name: "Dhain Iskander", age:54 + " years old", gender: "Male"},
+            {name: "Serdar Taskin", age:41 + " years old", gender: "Male"},
+            {name: "Christian Noel Assumbrado", age:21 + " years old", gender: "Male"},
+            {name: "Jesie James Ampaw", age:17 + " years old", gender: "Male"},
+            {name: "Andrea Balot", age:18 + " years old", gender: "Female"},
+        ];
+    }
+
+}
